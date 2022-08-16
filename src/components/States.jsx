cb2613782aeb3301d12b3eb528f91442255fd087
Abhishek Kumar <ak96166956@gmail.com>
2022-08-16 12:30:55 +0530

    Final commit

---
+++ src/components/States.jsx
@@ -0,0 +1,15 @@
+import { stats } from '../constants';
+import styles from '../style';
+
+const States = () => (
+    <section className={`${styles.flexCenter} flex-wrap flex-row sm:mb-20 mb-6`}>
+      {stats.map((stat) => (
+        <div key={stat.id} className="flex-1 flex justify-start items-center flex-row m-3">
+          <h4 className="font-poppins font-semibold xs:text-[40px] text-[30px] leading-[43px] xs:leading-[53px] text-white ">{stat.value}</h4>
+          <p className="font-poppins font-normal xs:text-[20px] text-[15px] leading-[21px] xs:leading-[26px] text-gradient uppercase ml-3">{stat.title}</p>
+        </div>
+      ))}
+    </section>
+  );
+
+export default States
---
+++ src/components/index.js
@@ -0,0 +1,24 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import States from "./States";
+import CTA from "./CTA";
+import Testimonials from "./Testimonials";
+import Footer from "./Footer";
+
+
+export {
+    Navbar,
+    Hero,
+    Billing,
+    Business,
+    CardDeal,
+    Clients,
+    States,
+    CTA,
+    Testimonials,
+    Footer
+}
